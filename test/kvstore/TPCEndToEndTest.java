5a5e8eddc6e1c5429e40498e87fd99a34c24826d
jxwuyi <jxwuyi@gmail.com>
2014-06-16 16:56:24 +0800

    add one multiPutGet test case to TPCEndToEndTest

--- test/kvstore/TPCEndToEndTest.java
+++ test/kvstore/TPCEndToEndTest.java
@@ -1,15 +1,34 @@
 package kvstore;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class TPCEndToEndTest extends TPCEndToEndTemplate {
 
     @Test(timeout = 15000)
-    public void testPutGet() throws KVException {
+    public void testSinglePutGet() throws KVException {
         client.put("foo", "bar");
         assertEquals("get failed", client.get("foo"), "bar");
+    }
+    
+    @Test(timeout = 15000)
+    public void testMultiplePutGet() throws KVException {
+    	try {
+	        client.put("foo", "bar");
+	        assertEquals("get failed", client.get("foo"), "bar");
+	        client.put("foo", "okay");
+	        assertEquals(client.get("foo"), "okay");
+	        assertNull(client.get("okay"));
+	        client.del("foo");
+	        assertNull(client.get("foo"));
+	        client.put("foo", "1");
+	        assertEquals("1", client.get("foo"));
+	        client.put("okay", "correct");
+	        assertEquals("correct", client.get("okay"));
+    	} catch (Exception e) {
+    		fail("Error Occurred!");
+    	}
     }
 
 }
